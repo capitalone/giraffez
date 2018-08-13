ce80e40df416840a4776779240edd0d645a64002
Chris Marshall <christopher.marshall@capitalone.com>
2018-08-13 16:24:18 -0400

    Fixes #50

--- giraffez/__init__.py
+++ giraffez/__init__.py
@@ -29,7 +29,7 @@ user-friendly and very fast.
 """
 
 __title__ = 'giraffez'
-__version__ = '2.0.22'
+__version__ = '2.0.23'
 __authors__ = ['Christopher Marshall', 'Kyle Travis']
 __license__ = 'Apache 2.0'
 __all__     = ['BulkExport', 'BulkLoad', 'Cmd', 'Config', 'Secret']
